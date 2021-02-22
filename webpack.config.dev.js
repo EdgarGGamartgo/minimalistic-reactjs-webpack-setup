86d03b97d6d27731437caf9cc9582a793be0de65
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-02-22 17:08:18 -0600

    Init commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dev'),
+  },
+  devtool: 'eval-source-map',
+  watch: false,
+  resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dev'),
+    compress: true,
+    port: 9000,
+    open: true
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|png)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]?[hash]'
+        }
+      }
+    ]
+  }
+}
